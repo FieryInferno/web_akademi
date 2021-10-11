31a6f78a61292e05fed2359db4a7b448a3d056ef
FieryInferno <bagassetia271@gmail.com>
2021-10-11 18:31:21 +0700

    Menampilkan halaman awal

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,258 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="zxx">
+
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>A COMPUTER ACADEMY</title>
+  <link rel="icon" href="img/acompdemy4.png">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/bootstrap.min.css">
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/animate.css">
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/owl.carousel.min.css">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/all.css">
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/flaticon.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/themify-icons.css">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/magnific-popup.css">
+  <!-- swiper CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/slick.css">
+  <!-- style CSS -->
+  <link rel="stylesheet" href="<?= base_url(); ?>asset/css/style.css">
 </head>
+
 <body>
+  <!--::header part start::-->
+  <header class="main_menu">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand main_logo" href="<?= base_url(); ?>"> <img src="<?= base_url(); ?>asset/img/acompdemy3.png" width="200" alt="logo"></a>
+            <a class="navbar-brand single_page_logo" href="<?= base_url(); ?>"> <img src="img/acompdemy3.png" alt="logo"> </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse"
+              data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="menu_icon"></span>
+            </button>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+            <div class="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">about</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <a href="register.php" class="d-none d-sm-block btn_1 home_page_btn">sign up</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--::Header part end::-->
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    <!--::banner part start::-->
+    <section class="banner_part">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-lg-5">
+                    <div class="banner_img d-none d-lg-block">
+                        <img src="img/banner_img.png" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+                            <h1>Made For Learning</h1>
+                            <p>A Computer Academy is a web academy created for anyone who wants to learn about
+                                computers. understanding computer science is very important for current developments
+                            </p>
+                            <a href="Siswa/dashboard.html" class="btn_2">Let's Learn</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="img/animate_icon/Ellipse_7.png" alt="" class="feature_icon_1 custom-animation1">
+        <img src="img/animate_icon/Ellipse_8.png" alt="" class="feature_icon_2 custom-animation2">
+        <img src="img/animate_icon/Ellipse_1.png" alt="" class="feature_icon_3 custom-animation3">
+        <img src="img/animate_icon/Ellipse_2.png" alt="" class="feature_icon_4 custom-animation4">
+        <img src="img/animate_icon/Ellipse_3.png" alt="" class="feature_icon_5 custom-animation5">
+        <img src="img/animate_icon/Ellipse_4.png" alt="" class="feature_icon_6 custom-animation6">
+    </section>
+    <!--::banner part start::-->
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+    <!--::use sasu part end::-->
+    <section id="about" class="use_sasu padding_top">
+        <div  class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single_feature">
+                        <div class="single_feature_part">
+                            <img src="img/a8.png" alt="">
+                            <h4>Hardware</h4>
+                            <p>Computer hardware includes the physical parts of a computer, such as the case, central
+                                processing unit, monitor, mouse, keyboard, computer data storage, graphics card, sound
+                                card, speakers and motherboard. By contrast, software is the set of instructions that
+                                can be stored and run by hardware. </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="single_feature">
+                        <div class="single_feature_part">
+                            <img src="img/a6.png" alt="">
+                            <h4>Network</h4>
+                            <p>A computer network can be defined as an interconnected collection of autonomous
+                                computers. It is a set of computers sharing resources located on or provided by network
+                                nodes. The computers use common communication protocols over digital interconnections to
+                                communicate with each other.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="img/animate_icon/Ellipse_1.png" alt="" class="feature_icon_1 custom-animation1">
+    </section>
+    <!--::use sasu part end::-->
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+    <!--::about_us part start::-->
+    <section class="about_us section_padding">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-md-6 col-lg-5">
+                    <div class="about_us_text">
+                        <h2>Hardware</h2>
+                        <p>computer hardware is the physical components that a computer system requires to function. It
+                            encompasses everything with a circuit board that operates within a PC or laptop; including
+                            the motherboard, graphics card, CPU (Central Processing Unit), ventilation fans, webcam,
+                            power supply, and so on.</p>
+                        <a href="Siswa/dashboard.html" class="btn_2">Let's Learn</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                    <div class="learning_img">
+                        <img src="img/a5.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="img/animate_icon/Ellipse_4.png" alt="" class="feature_icon_1 custom-animation1">
+    </section>
+    <!--::about_us part end::-->
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
+    <!--::about_us part start::-->
+    <section class="about_us right_time">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-md-6 col-lg-6">
+                    <div class="learning_img">
+                        <img src="img/a2.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-5">
+                    <div class="about_us_text">
+                        <h2>Network</h2>
+                        <p>A network consists of two or more computers that are linked in order to share resources (such
+                            as printers and CDs), exchange files, or allow electronic communications. The computers on a
+                            network may be linked through cables, telephone lines, radio waves, satellites, or infrared
+                            light beams.</p>
+                        <a href="Siswa/dashboard.html" class="btn_2">Let's Learn</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="img/animate_icon/Ellipse_5.png" alt="" class="feature_icon_2 custom-animation2">
+    </section>
+    <!--::about_us part end::-->
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+    <!--::footer_part start::-->
+    <footer id="contact" class="footer_part">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-md-4 col-lg-10">
+                    <div class="single_footer_part">
+                        <a href="index.html" class="footer_logo_iner"> <img src="img/acompdemy 2.png" width="400"
+                                alt="#"> </a>
+                        <p>A Computer Academy is the place to learn all about computer basics. This computer academy will
+                            help you to better understand computers and get to know computers. here you can learn
+                            computers from basic things to learn how computers work.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-md-3 col-lg-2">
+                    <div class="single_footer_part">
+                        <h4>Contact</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="https://www.instagram.com/?hl=id">Instagram</a></li>
+                            <li><a href="https://twitter.com/ayudhar_">Twitter</a></li>
+                            <li><a href="https://api.whatsapp.com/send?phone=6281390554506">Whatsapp</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="copyright_text">
+                        <P>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;
+                            <script>document.write(new Date().getFullYear());</script> A Computer Academy
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </P>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="footer_icon social_icon">
+                        <ul class="list-unstyled">
+                            <li><a href="https://www.instagram.com/?hl=id" class="single_social_icon"><i
+                                        class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://twitter.com/ayudhar_" class="single_social_icon"><i
+                                        class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://api.whatsapp.com/send?phone=6281390554506"
+                                    class="single_social_icon"><i class="fas fa-phone"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--::footer_part end::-->
 
+    <!-- jquery plugins here-->
+    <script src="<?= base_url(); ?>asset/js/jquery-1.12.1.min.js"></script>
+    <!-- popper js -->
+    <script src="<?= base_url(); ?>asset/js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="<?= base_url(); ?>asset/js/bootstrap.min.js"></script>
+    <!-- easing js -->
+    <script src="<?= base_url(); ?>asset/js/jquery.magnific-popup.js"></script>
+    <!-- swiper js -->
+    <script src="<?= base_url(); ?>asset/js/swiper.min.js"></script>
+    <!-- swiper js -->
+    <script src="<?= base_url(); ?>asset/js/masonry.pkgd.js"></script>
+    <!-- particles js -->
+    <script src="<?= base_url(); ?>asset/js/owl.carousel.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/jquery.nice-select.min.js"></script>
+    <!-- slick js -->
+    <script src="<?= base_url(); ?>asset/js/slick.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/jquery.counterup.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/waypoints.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/contact.js"></script>
+    <script src="<?= base_url(); ?>asset/js/jquery.ajaxchimp.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/jquery.form.js"></script>
+    <script src="<?= base_url(); ?>asset/js/jquery.validate.min.js"></script>
+    <script src="<?= base_url(); ?>asset/js/mail-script.js"></script>
+    <!-- custom js -->
+    <script src="<?= base_url(); ?>asset/js/custom.js"></script>
 </body>
+
 </html>
