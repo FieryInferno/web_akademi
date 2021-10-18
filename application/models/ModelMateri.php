b00c83c0999f48c86c51edaf552718d8af489c86
FieryInferno <bagassetia271@gmail.com>
2021-10-18 13:27:17 +0700

    Perbaikan pengambilan data materi

--- application/models/ModelMateri.php
+++ application/models/ModelMateri.php
@@ -19,6 +19,7 @@ class ModelMateri extends CI_Model {
   public function getByIdKelas($id_kelas)
   {
     $this->db->join('kelas', 'materi.kelas_id = kelas.id');
+    $this->db->select('materi.id as id_materi, judul_materi, keterangan, kelas_id, isi_materi');
     return $this->db->get_where('materi', ['kelas_id' => $id_kelas])->result_array();
   }
 
