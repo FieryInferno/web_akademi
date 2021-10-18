0d84cbc16f44fe6e1bbab0ad1d7de9eb8fe8f251
FieryInferno <bagassetia271@gmail.com>
2021-10-18 17:07:39 +0700

    Menghitung jumlah completion pada materi masing - masing kelas

--- application/models/ModelKelas.php
+++ application/models/ModelKelas.php
@@ -43,14 +43,14 @@ class ModelKelas extends CI_Model {
   {
     $data = $this->db->get_where('kelas', ['id' => $id_kelas])->row_array();
     
-    $materi = $this->db->get_where('materi', ['kelas_id' => $id_kelas])->result_array();
+    $materi           = $this->db->get_where('materi', ['kelas_id' => $id_kelas])->result_array();
+    $totalKelasSiswa  = $this->db->get_where('kelas_siswa', ['kelas_id' => $id_kelas])->num_rows();
 
     for ($i=0; $i < count($materi); $i++) {
-      $key      = $materi[$i];
+      $key  = $materi[$i];
       $progress = $this->db->get_where('progress_siswa', ['materi_id' => $key['id']])->result_array();
-      $total    = $this->db->get_where('materi', ['kelas_id' => $id_kelas])->result_array();
       
-      $materi[$i]['completion'] = count($progress) / count($total) * 100;
+      $materi[$i]['completion'] = round(count($progress) / $totalKelasSiswa * 100, 2);
     }
     
     $data['materi'] = $materi;
